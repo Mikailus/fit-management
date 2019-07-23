ac2a2f8900e5d3fc399b18e21ca5f337247485a6
Michał Wajer <wajer.michael@gmail.com>
2019-07-23 22:38:43 +0200

    FM-Nutrition Basic view styling with hardcoded data

---
+++ src/components/modules/nutrition/DailyIntake.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <daily-intake-summary></daily-intake-summary>
+        <daily-intake-meal></daily-intake-meal>
+        <daily-intake-meal></daily-intake-meal>
+        <daily-intake-meal></daily-intake-meal>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import moment, { Moment } from 'moment';
+
+import { NutritionAPI } from '../../../api/nutrition';
+
+import DailyIntakeMeal from './DailyIntakeMeal.vue';
+import DailyIntakeSummary from './DailyIntakeSummary.vue';
+
+@Component({
+  components: {
+    'daily-intake-meal': DailyIntakeMeal,
+    'daily-intake-summary': DailyIntakeSummary
+  },
+})
+export default class DailyIntake extends Vue {
+    @Prop({default: undefined}) day!: Moment;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
